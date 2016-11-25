1a569bd10f74c5e4a7d9ab92a9b66e5241addd6f
Vitaliy Karakushan <karakushan@gmail.com>
2016-11-25 12:10:42 +0200

    добавил плагин для создания миниатюр

--- path-gallery.php
+++ path-gallery.php
@@ -26,6 +26,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+require_once('BFI_Thumb.php');
+ @define( BFITHUMB_UPLOAD_DIR,'shop');
 
 /**
  * @param $path - относительный путь к папке с файлами
@@ -38,20 +40,35 @@ function path_gallery_files($path)
     // поэтому сбрасываем кэш.
     clearstatcache();
     $full_path=$_SERVER['DOCUMENT_ROOT'].$path;
-
     if (empty($path) || !file_exists($full_path)) return array();
-
     $files = scandir($full_path);
-
     for($i = 0, $length = count($files); $i < $length; $i++)
     {
-        // Исключаем из списка директории:
-        if( is_dir( $full_path.$files[$i]) )
-        {
+        // Исключаем из списка директории и не изображения:
+        if( is_dir( $full_path.$files[$i]) || !checkValidFormat($files[$i], array('jpg','jpeg','png','gif','bmp','svg')) || strpos($files[$i],'thumb')){
             unset($files[$i]);
+        }else{
+            $files[$i]=$path.$files[$i];
         }
     }
     return $files; //array
+}
+
+/**
+* Проверка корректности формата файла
+* 
+* @param string $file - имя файла или путь до файла
+* @param array $validFormat - массив с корректными форматами
+*
+* @return boolean - результат проверки
+*/
+function checkValidFormat($file, $validFormat){
+    $file_el=explode(".",$file);
+    $format = end($file_el);
+    if(in_array(strtolower($format), $validFormat)){
+        return true;
+    }
+    return false;
 }
 
 
