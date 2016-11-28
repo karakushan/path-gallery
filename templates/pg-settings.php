722adebbefef8b8420e7cf15fcaeeedbe86c5b97
Vitaliy Karakushan <karakushan@gmail.com>
2016-11-28 10:43:29 +0200

    добавил страницу настроек

---
+++ templates/pg-settings.php
@@ -0,0 +1,20 @@
+<div class="wrap">
+	<h1><?php _e( 'Settings galleries', 'path-gallery'); ?></h1>
+	<form method="post" name="path-gallery" class="pg-form">
+		<table class="form-table">
+			<tr>
+				<th scope="row">
+					<label for="ispmanager_path"><?php _e('Главная папка галерей', 'path-gallery' ); ?></label>
+				</th>
+				<td>
+					<input type="text" name="path_gallery[galleries_path]" class="regular-text" id="galleries_path" placeholder="<?php _e('for example /images/', 'path-gallery' ); ?>" value="<?php echo $this->settings['galleries_path'] ?>">
+				</td>
+			</tr>
+		</table>
+		<div class="form-group row">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="button button-primary customize load-customize hide-if-no-customize"><?php _e('Save', 'path-gallery' ); ?></button>
+			</div>
+		</div>
+	</form>
+</div>
